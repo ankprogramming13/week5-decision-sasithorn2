c87dacf9124c170ac4a9f05a45aac71478633f78
Unknown <toeyhom64@gmail.com>
2017-11-28 17:44:39 +0700

    1

--- lab_5_1.cpp
+++ lab_5_1.cpp
@@ -16,9 +16,14 @@ int main ()
     cout << "Enter the second name: ";
     cin >> name2;
 
-    // เขียนเงื่อนไขให้ถูกต้องซะ หนูๆ
+    if (name1 == name2) {
     cout << "The two names are equal" << endl;
+    }else{
     cout << "The two names are not equal" << endl;
+    }
+
+    // เขียนเงื่อนไขให้ถูกต้องซะ หนูๆ
+
 
     return 0;
 }
